5dd5fe5bab7cab5bf6715cf93cd4d46dc1fc3e62
Nicholas Gates <gatesn@users.noreply.github.com>
2018-05-09 15:52:11 +0100

    Implement assertj representation (#39)
    * Implement assertj representation
    
    * Add tests and fix complex types
    
    * Add tests and fix complex types

---
+++ preconditions-assertj/src/main/java/com/palantir/logsafe/testing/LoggableArgRepresentation.java
@@ -0,0 +1,36 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.logsafe.testing;
+
+import com.palantir.logsafe.Arg;
+import org.assertj.core.presentation.StandardRepresentation;
+
+public class LoggableArgRepresentation extends StandardRepresentation  {
+
+    @Override
+    protected final String fallbackToStringOf(Object object) {
+        if (object instanceof Arg) {
+            Arg<?> arg = (Arg<?>) object;
+            return String.format("%s[%s=%s]",
+                    arg.getClass().getSimpleName(),
+                    arg.getName(),
+                    toStringOf(arg.getValue()));
+        }
+        return super.fallbackToStringOf(object);
+    }
+
+}

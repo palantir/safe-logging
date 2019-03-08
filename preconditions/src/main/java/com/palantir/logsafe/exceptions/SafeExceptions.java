b513fe4af4d0a2cecf9d0cd44d0befa25d8c8cff
David Schlosnagle <davids@palantir.com>
2019-03-08 14:23:38 -0500

    Remove Guava MoreObjects dependency

--- preconditions/src/main/java/com/palantir/logsafe/exceptions/SafeExceptions.java
+++ preconditions/src/main/java/com/palantir/logsafe/exceptions/SafeExceptions.java
@@ -16,8 +16,8 @@
 
 package com.palantir.logsafe.exceptions;
 
-import com.google.common.base.MoreObjects;
 import com.palantir.logsafe.Arg;
+import java.util.Arrays;
 
 /**
  * {@link SafeExceptions} provides utility functionality for SafeLoggable exception implementations.
@@ -30,11 +30,30 @@ final class SafeExceptions {
             return message;
         }
 
-        MoreObjects.ToStringHelper builder = MoreObjects.toStringHelper(message + ": ");
-        for (Arg<?> arg : args) {
-            builder.add(arg.getName(), arg.getValue());
+        StringBuilder builder = new StringBuilder();
+        builder.append(message).append(": {");
+        for (int i = 0; i < args.length; i++) {
+            Arg<?> arg = args[i];
+            if (i > 0) {
+                builder.append(", ");
+            }
+
+            builder.append(arg.getName()).append("=");
+            appendValue(builder, arg);
         }
+        builder.append('}');
 
         return builder.toString();
     }
+
+    private static void appendValue(StringBuilder builder, Arg<?> arg) {
+        Object value = arg.getValue();
+        if (value != null && value.getClass().isArray()) {
+            String arrayString = Arrays.deepToString(new Object[] {value});
+            builder.append(arrayString, 1, arrayString.length() - 1);
+        } else {
+            builder.append(value);
+        }
+    }
+
 }

74996848e29575cadcaaca89d053677d598cfff3
Mattheo Ioannou <mioannou@palantir.com>
2021-02-25 15:09:50 +0100

    add iterables class + tests

---
+++ iterables/src/main/java/com/palantir/logsafe/Iterables.java
@@ -0,0 +1,42 @@
+/*
+ * (c) Copyright 2018 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (C) 2007 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.palantir.logsafe;
+
+import com.google.common.collect.Iterators;
+import java.util.Collection;
+
+public final class Iterables {
+    private Iterables() {}
+
+    public static <T, C extends Collection<T>> T getOnlyElement(C collection, Arg<?>... args) {
+        Preconditions.checkState(collection.size() == 1, "Expected exactly one element in collection", args);
+        return Iterators.getOnlyElement(collection.iterator());
+    }
+}
---
+++ iterables/src/test/java/com/palantir/logsafe/IterablesTest.java
@@ -0,0 +1,46 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.logsafe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.google.common.collect.ImmutableSet;
+import com.palantir.logsafe.exceptions.SafeIllegalStateException;
+import org.junit.Test;
+
+public class IterablesTest {
+    @Test
+    public void getOnlyElementReturnsElement() {
+        String element = "element";
+        assertThat(Iterables.getOnlyElement(ImmutableSet.of(element))).isEqualTo(element);
+    }
+
+    @Test
+    public void getOnlyElementThrowsIfMoreThanOneElement() {
+        String element1 = "element1";
+        String element2 = "element2";
+        assertThatThrownBy(() -> Iterables.getOnlyElement(ImmutableSet.of(element1, element2)))
+                .isInstanceOf(SafeIllegalStateException.class);
+    }
+
+    @Test
+    public void getOnlyElementThrowsIfEmpty() {
+        assertThatThrownBy(() -> Iterables.getOnlyElement(ImmutableSet.of()))
+                .isInstanceOf(SafeIllegalStateException.class);
+    }
+}

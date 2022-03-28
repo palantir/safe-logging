627e33a4b54515cbb5e951903db32a716f827660
Carter Kozak <ckozak@palantir.com>
2022-03-28 14:25:32 -0400

    Apply safety annotations to safe-logging types

--- safe-logging/src/main/java/com/palantir/logsafe/UnsafeArg.java
+++ safe-logging/src/main/java/com/palantir/logsafe/UnsafeArg.java
@@ -25,7 +25,7 @@ public final class UnsafeArg<T> extends Arg<T> {
         super(name, value);
     }
 
-    public static <T> UnsafeArg<T> of(String name, @Nullable @Unsafe T value) {
+    public static <T> UnsafeArg<T> of(@Safe String name, @Nullable @Unsafe T value) {
         return new UnsafeArg<>(name, value);
     }
 

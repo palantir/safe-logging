87043d719c915efe5336556e6284b2e8a0868ae9
Patrick Koenig <pkoenig10@gmail.com>
2025-07-21 10:38:06 -0400

    Fix suppressed warnings

--- logger-generator/src/main/java/com/palantir/logsafe/logger/generator/LoggerGenerator.java
+++ logger-generator/src/main/java/com/palantir/logsafe/logger/generator/LoggerGenerator.java
@@ -41,7 +41,7 @@ import javax.annotation.Nullable;
 import javax.annotation.processing.Generated;
 import javax.lang.model.element.Modifier;
 
-@SuppressWarnings("checkstyle:CyclomaticComplexity")
+@SuppressWarnings("CyclomaticComplexity")
 public final class LoggerGenerator {
 
     private static final ClassName BRIDGE_NAME = ClassName.get("com.palantir.logsafe.logger.spi", "SafeLoggerBridge");
@@ -374,22 +374,22 @@ public final class LoggerGenerator {
         return spec.parameters.stream().anyMatch(param -> Objects.equals(param.type, THROWABLE_TYPE));
     }
 
-    @SuppressWarnings("for-rollout:StringConcatToTextBlock")
     private static String copyright(int year) {
         return String.format(
-                "(c) Copyright %d Palantir Technologies Inc. All rights reserved.\n"
-                        + "\n"
-                        + "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
-                        + "you may not use this file except in compliance with the License.\n"
-                        + "You may obtain a copy of the License at\n"
-                        + "\n"
-                        + "    http://www.apache.org/licenses/LICENSE-2.0\n"
-                        + "\n"
-                        + "Unless required by applicable law or agreed to in writing, software\n"
-                        + "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
-                        + "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
-                        + "See the License for the specific language governing permissions and\n"
-                        + "limitations under the License.",
+                """
+                (c) Copyright %d Palantir Technologies Inc. All rights reserved.
+
+                Licensed under the Apache License, Version 2.0 (the "License");
+                you may not use this file except in compliance with the License.
+                You may obtain a copy of the License at
+
+                    http://www.apache.org/licenses/LICENSE-2.0
+
+                Unless required by applicable law or agreed to in writing, software
+                distributed under the License is distributed on an "AS IS" BASIS,
+                WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                See the License for the specific language governing permissions and
+                limitations under the License.""",
                 year);
     }
 
